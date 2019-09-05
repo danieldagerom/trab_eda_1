1b824bfd1977a5cd14954f868aebfc761925da2d
Daniel Primo <danielprimo9@gmail.com>
2019-09-05 13:38:14 -0300

    continuacao desse trabalho incrivel obrigado senhor

--- trabalho1.c
+++ trabalho1.c
@@ -4,90 +4,109 @@ typedef struct Funcionario{
     int idade;
     char *nome, *email;
 }funcionario;
-void criafuncionario(int qtd);
+funcionario *criafuncionario(int quant);
+int cadastro(int *quant, int *novo);
 void incluifuncionario();
-void imprimefuncionario(int qtd);
+void imprimefuncionario(funcionario *ptr, int quant, int comp);
 void menu();
 int main(){
-    funcionario *ptr;
-    int qtd, i, qtd2;
     menu();
     
     return 0;
 }
 void menu(){
-    int opcao;
+    funcionario *empregado;
+    int opcao = -1, quant = 0, novo = 0;
+    while(opcao != 0){
+        printf("Seja bem vindo ao primeiro trabalho de EDA1!\n");
+        printf("____________________________________________\n");
+        printf("________Esse eh o MENU DE OPCOES:___________\n");
+        printf("1) *INCLUIR* FUNCIONARIOS:\n");
+        printf("2) *LISTAR* FUNCIONARIOS INCLUIDOS:\n");
+        printf("3) *EDITAR* FUNCIONARIOS INCLUIDOS:\n");
+        printf("4) *EXCLUIR* FUNCIONARIOS INCLUIDOS:\n");
+        printf("0) *SAIR*:\n");
+        printf("Qual opçao voce deseja? -> ");
 
-    printf("Seja bem vindo ao primeiro trabalho de EDA1!\n");
-    printf("____________________________________________\n");
-    printf("________Esse eh o MENU DE OPCOES:___________\n");
-    printf("1) *INCLUIR* FUNCIONARIOS:\n");
-    printf("2) *LISTAR* FUNCIONARIOS INCLUIDOS:\n");
-    printf("3) *EDITAR* FUNCIONARIOS INCLUIDOS:\n");
-    printf("4) *EXCLUIR* FUNCIONARIOS INCLUIDOS:\n");
-    printf("0) *SAIR*:\n");
+        scanf("%d", &opcao);
 
-
-    printf("Qual opçao voce deseja? -> ");
-    scanf("%d", &opcao);
-    switch (opcao)
-    {
-    case 1:
-        incluifuncionario();
-        break;
-    
-    default:
-        break;
+        switch (opcao)
+        {
+        case 1:
+            quant += cadastro(&quant, &novo);
+            empregado = criafuncionario(quant);
+            incluifuncionario(&empregado[0], quant);
+            break;
+        case 2:
+            imprimefuncionario(empregado ,quant, novo);
+        
+        default:
+            break;
+        }
     }
-}
-void incluifuncionario(){
-    int qtd, i;
-    funcionario *ptr;
-    printf("Quantos alunos deseja incluir?\n");
-    scanf("%d", &qtd);
-    criafuncionario(qtd);
-    for(i = 0; i < qtd; i++){
-        printf("Digite o nome do funcionario[%d] -> ", i);
-        scanf("%[^\n", ptr[i].nome);
-        printf("\nDigite o email do funcionario[%d] -> ", i);
-        scanf("%[^\n", ptr[i].email);
-        printf("\nDigite a idade do funcionario[%d] -> ", i);
-        scanf("%[^\n", ptr[i].idade);
+    for(int i = 0; i < quant; i++){
+        free(empregado[i].nome);
+        printf("limpou nome!\n ");
+        free(empregado[i].email);
+        printf("limpou email!\n ");
     }
+    free(empregado);
+
 }
-void imprimefuncionario(int qtd){
-    funcionario *ptr;
+void incluifuncionario(funcionario *ptr, int *quant){
     int i;
-    for(int i = 0; i < qtd; i++){
-        printf("%[^\n] -- %[^\n] -- %d\n", ptr[i].nome, ptr[i].email, ptr[i].idade);
+    getchar(); 
+    for(i = 0; i < quant; i++){
+        printf("\nDigite o nome do funcionario[%d] -> ", i + 1);
+        scanf("%[^\n]", ptr[i].nome);
+        printf("\nDigite o email do funcionario[%d] -> ", i + 1);
+        getchar();
+        scanf("%[^\n]", ptr[i].email);
+        printf("\nDigite a idade do funcionario[%d] -> ", i + 1);
+        scanf("%d", &ptr[i].idade);        
     }
 }
-
-void criafuncionario(int qtd){
-    funcionario *ptr;
+void imprimefuncionario(funcionario *ptr, int quant, int comp){
     int i;
+    for(int i = comp; i < quant; i++){
+        printf("     %s -- %s -- %d\n", ptr[i].nome, ptr[i].email, ptr[i].idade);
+    }
+}
+int cadastro(int *quant, int *novo){
+    *novo = *quant;
+    printf("Quantos funcionarios deseja incluir?\n");
+    scanf("%d", quant);
 
-    ptr = (funcionario *) malloc(qtd * sizeof(funcionario));
+    return *novo;
+}
+
+
+
+funcionario *criafuncionario(int quant){
+    int i;
+    funcionario *ptr;
+    ptr = (funcionario *) malloc(quant * sizeof(funcionario));
     if(ptr == NULL){
         printf("ERRO!\n");
         exit(2);
     }
-    for(i = 0; i < qtd; i++){
-        ptr[i].nome = (char *) malloc(35 * sizeof(char));
+    for(i = 0; i < quant; i++){
+        ptr[i].nome = (char *) malloc(50 * sizeof(char));
         printf("deu certo   ");
         if(ptr[i].nome == NULL){
             printf("ERRO!\n");
             exit(2);
         } 
     }
-     for(i = 0; i < qtd; i++){
-        ptr[i].email = (char *) malloc(35 * sizeof(char));
+     for(i = 0; i < quant; i++){
+        ptr[i].email = (char *) malloc(50 * sizeof(char));
         printf("deu certo   ");
         if(ptr[i].email == NULL){
             printf("ERRO!\n");
             exit(2);
         } 
     }
+    return ptr;
 }
 
 
